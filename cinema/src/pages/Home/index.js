0b9966d742fbfbf72264d9f36165d38377cd0a74
RianDantas <134387076+RianDantas@users.noreply.github.com>
2024-11-14 18:21:16 +0000

    j

---
+++ cinema/src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import estilo from './home.module.css'
+function Home(){
+    return(
+        <div>
+           <h1 className={estilo.titulo}>home</h1>
+        </div>
+    );
+}
+
+export default Home;
